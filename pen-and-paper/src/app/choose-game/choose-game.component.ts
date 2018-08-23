dc0961c1c408e3825fa7473dbc57e7535565baf1
Frederik Michels <f.michels@traperto.com>
2018-08-23 19:33:20 +0200

    navbar frame

---
+++ pen-and-paper/src/app/choose-game/choose-game.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "choose-game",
+  templateUrl: "./choose-game.component.html",
+  styleUrls: ["./choose-game.component.scss"]
+})
+export class ChooseGameComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
