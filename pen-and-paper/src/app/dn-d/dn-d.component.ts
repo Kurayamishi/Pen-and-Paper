2271a1c41363dc0bb7aa96f82127da0f5777b91d
Frederik Michels <f.michels@traperto.com>
2018-09-29 23:01:54 +0200

    removed not used component. changed general style composition to grid and flex. finished base routing

--- pen-and-paper/src/app/dn-d/dn-d.component.ts
+++ pen-and-paper/src/app/dn-d/dn-d.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "DnD",
+  selector: "dnd",
   templateUrl: "./dn-d.component.html",
   styleUrls: ["./dn-d.component.scss"]
 })
 export class DnDComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 }
